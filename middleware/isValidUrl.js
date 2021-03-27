ca229033331dd8a13825064a9a2e962539a0b795
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-27 00:19:44 -0700

    'Update Middleware using req.body and proper validation'

--- middleware/isValidUrl.js
+++ middleware/isValidUrl.js
@@ -2,14 +2,18 @@ const validUrl = require("valid-url");
 const { isHttpUri, isHttpsUri } = validUrl;
 
 const isValidUrl = (req, res, next) => {
-  // Construct full URL
-  const uri = req.protocol + req.get("host") + req.originalUrl;
+  let uri = req.body.url;
+
+  // If uri not include in req.body then error
+  if (!uri) return res.status(400).json({ error: "Need a URL" });
 
   // Check if it is a valid URL
   if (isHttpUri(uri) || isHttpsUri(uri)) {
-    next();
+    return next();
   }
-  res.status(400).json({ error: "Not a valid Url" });
+
+  // if not valid url then error
+  return res.status(400).json({ error: "Not a valid URL" });
 };
 
 module.exports = isValidUrl;
