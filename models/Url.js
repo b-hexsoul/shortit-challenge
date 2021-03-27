8a99b491da0c3124e936eacafc9856120d79c6c9
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-27 00:20:06 -0700

    'Add count and validate'

--- models/Url.js
+++ models/Url.js
@@ -4,8 +4,19 @@ const { Schema } = mongoose;
 
 const urlSchema = new Schema({
   url_long: String,
-  url_short: String,
+  url_short: {
+    type: String,
+    validate: {
+      validator: (url_short) => Url.doesNotExist({ url_short }),
+      message: "Shorten URL already exists",
+    },
+  },
+  count: Number,
 });
+
+urlSchema.statics.doesNotExist = async function (field) {
+  return (await this.where(field).countDocuments()) === 0;
+};
 
 const Url = mongoose.model("Url", urlSchema);
 
